a41dc2d5b740befdac39a3e66b120635b364f8ff
Francis Russell <francis@unchartedbackwaters.co.uk>
2023-01-28 21:24:15 +0000

    Remove `Math.random`-based random number generator.

--- src/nonce.rs
+++ src/nonce.rs
@@ -1,9 +1,10 @@
-use crate::system::rng;
 use rustup_toolchain_manifest::HashValue;
 
 pub fn build(num_bytes: usize) -> HashValue {
+    use rand::RngCore as _;
+
     let mut bytes = vec![0u8; num_bytes];
-    let mut rng = rng::MathRandom::default();
+    let mut rng = rand::thread_rng();
     rng.fill_bytes(&mut bytes);
     HashValue::from_bytes(&bytes)
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,2 +1,1 @@
-pub(crate) mod rng;
 pub(crate) mod sleep;
